1951f4579c2893e198d5576534d92cab865e7f05
AyeshaaKhann <99113715+AyeshaaKhann@users.noreply.github.com>
2022-09-20 15:05:26 +0500

    Add files via upload

---
+++ chp-1,2,3,4/asset/app.js
@@ -0,0 +1,59 @@
+// alert("hello ayesha ");
+
+// var a = 30;
+// var b = 20;
+// if (a>b){
+//     alert("A is greater");
+// }else{
+//     alert("B is greater");
+// }
+
+//We can also concatinate
+// var a = 30;
+// var b = 20;
+// alert("value of A is: " + a)
+//we can perform mathemtic functions
+// var a = 10;
+// var b = 20;
+// alert(a+b);
+
+// var firstName = "ayesha"
+// var lastname = "khan"
+// var age = "18"
+// alert("Hello "  + firstName + " " + lastname + " " + "You Are" + " " + age + " " + "years Old")
+
+//same work as alert/prompt...
+// var city = prompt("Where Do Yoy Live?");
+// document.write(city);
+
+// var city = prompt("Where Do Yoy Live?");
+// alert(city);
+
+//Confirm Box
+var  a = confirm("Do u like Our Website?");
+if (a) {
+    document.write("Thanks For your likesness");
+
+}else{
+    alert("Sorry We Will improve..");
+}
+
+
+// //>===============================================
+//  var firstname = "ayesha khan";
+// document.write(firstname + "<br>");
+ 
+// var firstNmae = "Asif khan";
+// document.write(firstNmae);
+
+// let firstname = "ayesha khan";
+// document.write(firstname + "<br>");
+
+// let firstNmae = "asif khan";
+// document.write(firstNmae);
+
+// const salary = 500
+// const expences = 300;
+// document.write(expences + "<br>" + salary);
+
+
